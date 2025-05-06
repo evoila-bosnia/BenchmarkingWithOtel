10ff07437fb3bc7f7972fce3f761b68f7b28d130
damir-topic <89133183+damir-topic@users.noreply.github.com>
2025-05-06 12:35:11 +0200

    DT - playing with honeycomb

--- Main/Main.AppHost/Program.cs
+++ Main/Main.AppHost/Program.cs
@@ -1,3 +1,5 @@
+using Aspire.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var database = builder.AddOracle("oracledb").WithImageTag("latest-lite");
@@ -5,10 +7,18 @@ var database = builder.AddOracle("oracledb").WithImageTag("latest-lite");
 var proxy = builder.AddProject<Projects.BenchmarkingWithOtel_ReverseProxy>("benchmarkingwithotel-reverseproxy");
 
 var server = builder.AddProject<Projects.BenchmarkingWithOtel_Server>("benchmarkingwithotel-server")
+    .WithEnvironment("OTEL_SERVICE_NAME", "benchmarkingwithotel-server")
+    .WithEnvironment("OTEL_EXPORTER_OTLP_PROTOCOL", "http/protobuf")
+    .WithEnvironment("OTEL_EXPORTER_OTLP_ENDPOINT", "https://api.honeycomb.io")
+    .WithEnvironment("OTEL_EXPORTER_OTLP_HEADERS", "x-honeycomb-team=FqTl7veAc5wvA5mRUJQYVD")
     .WithReference(database)
     .WaitFor(database);
 
 builder.AddProject<Projects.BenchmarkingWithOtel_Client>("benchmarkingwithotel-client")
+    .WithEnvironment("OTEL_SERVICE_NAME", "benchmarkingwithotel-client")
+    .WithEnvironment("OTEL_EXPORTER_OTLP_PROTOCOL", "http/protobuf")
+    .WithEnvironment("OTEL_EXPORTER_OTLP_ENDPOINT", "https://api.honeycomb.io")
+    .WithEnvironment("OTEL_EXPORTER_OTLP_HEADERS", "x-honeycomb-team=FqTl7veAc5wvA5mRUJQYVD")
     .WithReference(server)
     .WithReference(proxy)
     .WaitFor(server)
